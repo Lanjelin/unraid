351f1838783a0dbcdee168286c2c35b5d555d6c6
lanjelin <6988319+Lanjelin@users.noreply.github.com>
2026-04-17 13:54:02 +0200

    fix: verify interactive mode

--- zsh-omz-persistent/unraid-zsh-omz.sh
+++ zsh-omz-persistent/unraid-zsh-omz.sh
@@ -5,13 +5,19 @@
 # This script is built for Unraid to use ZSH and oh-my-zsh with persistant history
 # It moves settings USB Flash Drive, and omz along with history to /mnt/user/appdata/zsh
 #
-# Prerequisites: 
+# Prerequisites:
 #     zsh installed (using NerdTools CA Plugin)
 #     oh-my-zsh installed https://ohmyz.sh/#install
 #
 echo
-if [ ! -f /bin/zsh ]; then echo "zsh not installed!"; exit; fi
-if [ ! -d /root/.oh-my-zsh ]; then echo "oh-my-zsh not installed!"; exit; fi
+if [ ! -f /bin/zsh ]; then
+  echo "zsh not installed!"
+  exit
+fi
+if [ ! -d /root/.oh-my-zsh ]; then
+  echo "oh-my-zsh not installed!"
+  exit
+fi
 OMZPATH="/mnt/user/appdata/zsh"
 echo "Please enter path to store omz and history (leave blank to use $OMZPATH):"
 read directory
@@ -22,7 +28,7 @@ if [ -n "$directory" ]; then
     OMZPATH=$directory
     exit 0
   else
-  	exit 0
+    exit 0
   fi
 fi
 echo
@@ -33,16 +39,16 @@ echo "Moving .oh-my-zsh to $OMZPATH"
 mkdir -p $OMZPATH
 cp -r /root/.oh-my-zsh $OMZPATH"/.oh-my-zsh" && rm -rf /root/.oh-my-zsh
 echo "Updating .zshrc with new paths - history-file moved to $OMZPATH/.zsh_history"
-echo "export HISTFILE=$OMZPATH/.zsh_history" >> /boot/config/.zshrc
-sed -i -e "s|\$HOME/.oh-my-zsh|${OMZPATH}/.oh-my-zsh|" /boot/config/.zshrc 
+echo "export HISTFILE=$OMZPATH/.zsh_history" >>/boot/config/.zshrc
+sed -i -e "s|\$HOME/.oh-my-zsh|${OMZPATH}/.oh-my-zsh|" /boot/config/.zshrc
 if [ -f /root/.zsh_history ]; then mv /root/.zsh_history $OMZPATH/.zsh_history; fi
 echo "Updating .bash_profile"
-echo "if [ -d $OMZPATH/.oh-my-zsh ]; then ( if [ -f /bin/zsh ]; then /bin/zsh; exit; fi ) fi" >> /root/.bash_profile
-echo 'if [ ! "$TMUX" = "" ]; then export TERM=xterm-256color; fi' >> /root/.zshrc
+echo "if [[ $- == *i* ]]; then if [ -d $OMZPATH/.oh-my-zsh ] && [ -x /bin/zsh ]; then /bin/zsh; exit; fi fi" >>/root/.bash_profile
+echo 'if [ ! "$TMUX" = "" ]; then export TERM=xterm-256color; fi' >>/root/.zshrc
 echo "Updating /boot/config/go"
-echo '# Launch zsh by default if its installed, and .oh-my-zsh is available' >> /boot/config/go
-echo "echo \"if [ -d $OMZPATH/.oh-my-zsh ]; then if [ -f /bin/zsh ]; then /bin/zsh; exit; fi fi\" >> /root/.bash_profile" >> /boot/config/go
-echo '# Linking .zshrc to ~/.zshrc' >> /boot/config/go
-echo 'ln -s /boot/config/.zshrc /root/.zshrc' >> /boot/config/go
+echo '# Launch zsh by default if its installed, and .oh-my-zsh is available' >>/boot/config/go
+echo "echo \"if [[ $- == *i* ]]; then if [ -d $OMZPATH/.oh-my-zsh ] && [ -x /bin/zsh ]; then /bin/zsh; exit; fi fi\" >>/root/.bash_profile" >>/boot/config/go
+echo '# Linking .zshrc to ~/.zshrc' >>/boot/config/go
+echo 'ln -s /boot/config/.zshrc /root/.zshrc' >>/boot/config/go
 echo
 echo "Done, relaunch terminal to use the updated zsh"
